f51d04900fe7e915a6dca83b0f6187d8758ab5d6
BuildTools <ktakashi19980515@gmail.com>
2017-03-13 11:28:32 +0900

    Enable shortcut key

--- main.js
+++ main.js
@@ -2,19 +2,44 @@
 
 var electron = require('electron')
 var menubar = require('menubar')
+var Menu = electron.Menu
 
+if(process.env.npm_package_config_mode == 0) {
+    //---- build ----
+    var mb = menubar({})
+    var app = mb.app
+    var window = mb.window
+    var tray = mb.tray
 
-//---- use ----
-var mb = menubar({})
-var app = mb.app
-var window = mb.window
-var tray = mb.tray
+    mb.on('ready', function ready () {
+        console.log('app is ready')
+        var size = electron.screen.getPrimaryDisplay().workAreaSize;
+        WindowSetting(size.width/2, size.height/2, size.width)
+        //window.toggleDevTools();
+        Menu.setApplicationMenu(menu)
+    })
+} else {
+    //---Test---
+    var app = electron.app;
+    var BrowserWindow = electron.BrowserWindow;
 
-mb.on('ready', function ready () {
-    console.log('app is ready')
-    var size = electron.screen.getPrimaryDisplay().workAreaSize;
-    WindowSetting(size.width/2, size.height/2, size.width)
-})
+    var mainWindow = null;
+
+    app.on('window-all-closed', function() {
+        if (process.platform != 'darwin')
+            app.quit();
+    });
+
+    app.on('ready', function() {
+
+        mainWindow = new BrowserWindow({width: 800, height: 600});
+        mainWindow.loadURL('file://' + __dirname + '/index.html');
+            mainWindow.toggleDevTools();
+        mainWindow.on('closed', function() {
+            mainWindow = null;
+        });
+    });
+}
 
 function WindowSetting(width=-1, height=-1, x=-1, y=-1) {
     if(width != -1) mb.setOption("width", width)
@@ -23,25 +48,18 @@ function WindowSetting(width=-1, height=-1, x=-1, y=-1) {
     if(y != -1) mb.setOption("y", y)
 }
 
-//---Test---
-/*
-var app = electron.app;
-var BrowserWindow = electron.BrowserWindow;
-
-var mainWindow = null;
-
-app.on('window-all-closed', function() {
-    if (process.platform != 'darwin')
-        app.quit();
-});
-
-app.on('ready', function() {
-
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
-        mainWindow.toggleDevTools();
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
-});
-*/
+var menu = Menu.buildFromTemplate([
+    {
+        label: "Application",
+        submenu: [
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }},
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]
+    }
+])
