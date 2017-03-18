5725cb792c667392f05c6a173ed48fd6f0a669cf
BuildTools <ktakashi19980515@gmail.com>
2017-03-18 16:07:31 +0900

    Add function Dynamically add tab

--- window.js
+++ window.js
@@ -1,11 +1,34 @@
 'use strict';
 
 var activetab = 0
-var maxtab = 2
+var maxtabs = 2
 var dom = []
 var input = null
+var homepageurl = "https://google.co.jp"
 
 window.onload = function () {
+    var fragment = document.createDocumentFragment()
+    for(var i = 0;i < maxtabs;i++) {
+        var element = document.createElement("input")
+        element.id = "tab" + (i + 1)
+        element.className = "hide"
+        element.type = "radio"
+        element.name = "tab"
+        element.setAttribute("onclick", "ChangeTab("+i+")")
+        if(i == 0) {
+            element.checked = true
+        }
+        fragment.appendChild(element)
+    }
+    document.body.insertBefore(fragment, document.getElementById("back"))
+    fragment = document.createDocumentFragment()
+    for(var i = 0;i < maxtabs;i++) {
+        var label = document.createElement("label")
+        label.setAttribute("for", "tab" + (i + 1))
+        label.textContent = "Tab" + (i + 1)
+        fragment.appendChild(label)
+    }
+    document.getElementById("tabs").appendChild(fragment)
     dom.push(document.getElementById("view1"))
     dom.push(document.getElementById("view2"))
     input = document.getElementById("url")
@@ -17,21 +40,18 @@ window.onload = function () {
 }
 
 function KeyPressed(event) {
-    console.log(event.target.value)
     if(event.keyCode!=13) return;
     dom[activetab].loadURL(event.target.value)
 }
 
 function Back() {
-    if(dom[activetab].canGoBack()) {
+    if(dom[activetab].canGoBack()) 
         dom[activetab].goBack()
-    }
 }
 
 function Forword() {
-    if(dom[activetab].canGoForward()) {
+    if(dom[activetab].canGoForward()) 
         dom[activetab].goForward()
-    }
 }
 
 function Reload() {
@@ -39,7 +59,7 @@ function Reload() {
 }
 
 function HomePage() {
-    dom[activetab].loadURL("http://www.google.co.jp")
+    dom[activetab].loadURL(homepageurl)
 }
 
 function Setting() {
@@ -47,6 +67,8 @@ function Setting() {
 }
 
 function ChangeTab(num) {
+    dom[activetab].classList.add("hide")
     activetab = num
+    dom[activetab].classList.remove("hide")
     input.value = dom[activetab].getURL()
 }
